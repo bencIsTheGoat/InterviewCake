2b57dfc6e68486ef391032aceb7ba2085355cf32
Ben Cutler <bcutler94@gmail.com>
2019-06-01 16:43:23 -0400

    Fix mergeSorted

--- arraysAndStrings/mergeSortedArrays.js
+++ arraysAndStrings/mergeSortedArrays.js
@@ -2,14 +2,19 @@ function mergeArrays(arr1, arr2) {
     let i = 0;
     let j = 0;
     const output = [];
-    while (i < arr1.length && j < arr2.length) {
-        if (arr1[i] <= arr2[j]) {
-            output.push(arr1[i]);
+    while (i < arr1.length || j < arr2.length) {
+        const ele1 = arr1[i] === undefined ? Infinity : arr1[i];
+        const ele2 = arr2[j] === undefined ? Infinity : arr2[j];
+        if (ele2 < ele1) {
+            output.push(ele2)
+            j++;
+        } else if (ele1 < ele2) {
+            output.push(ele1);
             i++;
         } else {
-            output.push(arr2[j]);
+            i++;
             j++;
         }
     }
-    return output.concat(arr1.slice(i)).concat(arr2.slice(j));
+    return output;
 }
