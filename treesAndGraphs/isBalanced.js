ab1f1d8f2937375548a82c114d12aa0e8f1f00fe
Ben Cutler <bcutler94@gmail.com>
2019-06-11 11:03:12 -0400

    Add isBalanced

--- treesAndGraphs/isBalanced.js
+++ treesAndGraphs/isBalanced.js
@@ -1,5 +1,26 @@
+function isBalanced(root) {
+    if (!root) return true;
+    const stack = [[root, 0]];
+    const depths = [];
+    while (stack.length > 0) {
+        const [node, depth] = stack.pop();
+        if (!node.right && !node.left) {
+            if (depths.indexOf(depth) < 0) {
+                depths.push(depth);
+                const magnitudeCheck = Math.abs(depths[0] - depths[1]) > 1;
+                const lenCheck = depths.length > 2;
+                if (lenCheck || magnitudeCheck) return false;
+            }
+        } else {
+            if (node.right) stack.push([node.right, depth + 1]);
+            if (node.left) stack.push([node.left, depth + 1]);
+        }
+    }
+    return true;
+}
 
 
+// recursive solution that doesnt work for LL edge case
 function isBalanced(treeRoot) {
     const min = getHeight(treeRoot, true);
     const max = getHeight(treeRoot, false);
