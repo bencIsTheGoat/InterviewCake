95f6d11d701898605ec5b67c002a35d19b4438e8
Ben Cutler <bcutler94@gmail.com>
2019-06-17 18:51:02 -0400

    Add bstDistance

--- treesAndGraphs/bstDistance.js
+++ treesAndGraphs/bstDistance.js
@@ -1,4 +1,35 @@
 function bfsGetPath(graph, startNode, endNode) {
+    if (!(endNode in graph)) throw new Error('no path')
+    const queue = [startNode];
+    const visited = new Set();
+    const pathObj = {};
+    visited.add(startNode);
+    while (queue.length > 0) {
+        const node = queue.shift();
+        visited.add(node);
+        if (node === endNode) {
+            return reconstructPath(pathObj, startNode, endNode);
+        }
+        for (const child of graph[node]) {
+            if (visited.has(child)) continue;
+            pathObj[child] = node;
+            queue.push(child);
+        }
+    }
+    return null;
+}
+
+function reconstructPath(obj, start, end) {
+    const output = [end];
+    let copy = end
+    while (start !== copy) {
+        copy = obj[copy];
+        output.push(copy);
+    }
+    return output.reverse();
+}
+
+function bfsGetPath(graph, startNode, endNode) {
     const val = { node: startNode, dist: 0, path: [startNode] };
     const max = [Infinity, null];
     const queue = [val];
