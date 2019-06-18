987c431ae9ac64e0982350e24a5d7772c30eab34
Ben Cutler <bcutler94@gmail.com>
2019-06-18 18:39:38 -0400

    Add findDups

--- treesAndGraphs/findDuplicates.js
+++ treesAndGraphs/findDuplicates.js
@@ -21,10 +21,11 @@ function findDuplicate(numbers) {
         fast = numbers[fast - 1];
         i++
     }
-    const start = fast;
     while (fast !== slow) {
         fast = numbers[fast - 1];
         slow = numbers[slow - 1];
     }
     return fast
 }
+
+console.log(findDuplicate([1, 13, 3, 4, 12, 6, 5, 10, 9, 8, 12, 12, 13, 2].reverse()))
