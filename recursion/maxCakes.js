267d73b2da09bd34d5436f58380a318a52bb9eeb
Ben Cutler <bcutler94@gmail.com>
2019-06-20 17:47:27 -0400

    Add maxcakes

--- recursion/maxCakes.js
+++ recursion/maxCakes.js
@@ -4,9 +4,7 @@ function maxDuffelBagValue(cakes, weight) {
         if (cake.weight === 0 && cake.value > 0) return Infinity;
         for (let i = cake.weight; i < cakeArr.length; i++) {
             const diff = i - cake.weight;
-            if (cakeArr[diff] + cake.value > cakeArr[i]) {
-                cakeArr[i] = cakeArr[diff] + cake.value;
-            }
+            cakeArr[i] = Math.max(cakeArr[i], cakeArr[diff] + cake.value)
         }
     }
     return cakeArr[cakeArr.length - 1];
